bafab79e4d2c0ec433a9c33ae21ebaf10c4d650e
Chanel-B <84045976+Chanel-B@users.noreply.github.com>
2022-09-20 00:19:14 +0200

    added user registration and login, there's an error home page not responding

---
+++ user-registration/src/main/java/com/userregistration/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.userregistration.service;
+
+import com.userregistration.entity.CustomUserDetails;
+import com.userregistration.entity.User;
+import com.userregistration.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+
+}
--- user-registration/src/main/resources/0010_create_user_table.sql
+++ user-registration/src/main/resources/0010_create_user_table.sql
@@ -1,2 +1,12 @@
 --create user table
-CREATE TABLE USER(Id integer PRIMARY KEY, Name VARCHAR(50));
+Hibernate:
+    CREATE TABLE USER(
+            id bigint not null auto_increment,
+            email varchar(45) not null,
+            first_name varchar(20) not null,
+            last_name varchar(20) not null,
+            password varchar(64) not null,
+            primary key (id)
+    );
+
+
